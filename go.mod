019e15d199c263fc19e625b5e704b2cd61afc47b
Yahav Rosner <rosneryahav@gmail.com>
2023-10-02 12:07:17 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rosnerdev/hello-go
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Hello(name string) {
+  fmt.Printf("Hello, %s!\n", name);
+}
+
+func Greet(name string) {
+  fmt.Printf("You're welcome, %s.\n", name);
+}
